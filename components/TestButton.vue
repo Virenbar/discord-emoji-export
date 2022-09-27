cb1bdd5d18ffef372da1db6785d28a3341f36024
Virenbar <rib.artem@gmail.com>
2022-09-27 11:24:52 +0500

    WIP

---
+++ components/TestButton.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <button>AAAAA</button>
+    </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+import { f } from "ohmyfetch/dist/error-f78c60df";
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+    typescript: {
+        shim: false
+    },
+    ssr:false
+})
