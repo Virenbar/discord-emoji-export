895c319f860b579130fb07ed27e822a39e3c757d
Virenbar <rib.artem@gmail.com>
2022-10-11 16:32:57 +0500

    WIP

--- src/components/EmojiList.tsx
+++ src/components/EmojiList.tsx
@@ -1,9 +1,10 @@
 import { APIEmoji } from "discord-api-types/v10";
+import _ from "lodash";
 import React from "react";
 import { Emoji } from "./Emoji";
 
 export function EmojiList(emojis: APIEmoji[]) {
-    const list = emojis.map(e =>
+    const list = _.sortBy(emojis, e => e.name).map(e =>
         <Emoji key={e.id} {...e} />
     );
     return (
---
+++ src/components/Export.tsx
@@ -0,0 +1,50 @@
+import { APIEmoji, RESTAPIPartialCurrentUserGuild } from "discord-api-types/v10";
+import React from "react";
+import { EmojiList } from "./EmojiList";
+
+export function Export(props: Props) {
+    const guild = props.guild;
+    return (
+        <div id="tabs" className="card">
+            <div className="card-header">
+                <ul className="nav nav-pills" role="tablist">
+                    <li className="nav-item" role="presentation">
+                        <a className="nav-link active" data-bs-toggle="tab" href="#export" aria-selected="true" role="tab">Export</a>
+                    </li>
+                    <li className="nav-item" role="presentation">
+                        <a className="nav-link" data-bs-toggle="tab" href="#browse" aria-selected="false" role="tab" tabIndex={-1}>Browse</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div className="card-body">
+                <div className="tab-content">
+                    <div className="tab-pane fade show active" role="tabpanel">
+                        <button className="btn btn-primary" onClick={exportJSON}>Download as JSON</button>
+                        <button className="btn btn-primary" onClick={exportZIP}>Download as ZIP</button>
+                    </div>
+                    <div className="tab-pane fade" role="tabpanel">
+                        <EmojiList {...props.emojis} />
+                    </div>
+                </div>
+            </div>
+
+            <div className="card-footer">
+                {`${props.emojis.length} emojis`}
+            </div>
+        </div>
+    );
+    function exportJSON(e: React.MouseEvent) {
+        console.log(e);
+        console.log(guild);
+    }
+    function exportZIP(e: React.MouseEvent) {
+        console.log(e);
+        console.log(guild);
+    }
+}
+
+interface Props {
+    guild: RESTAPIPartialCurrentUserGuild,
+    emojis: APIEmoji[]
+}
